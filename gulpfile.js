470c32ec2a03b83015664ebfd0088ae4a0c79434
Rachel Keslensky <rach@lid.cc>
2016-08-23 12:05:35 -0700

    Chore: add last of the major gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
+var del = require('del');
 var sass = require('gulp-sass');
 var gulpIf = require('gulp-if');
+var cache = require('gulp-cache');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var runSequence = require('run-sequence');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
@@ -37,4 +41,35 @@ gulp.task('useref', function(){
     // Minifies only if it's a CSS file
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
 });
