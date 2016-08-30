813de69423f8f91f497fb30746bf2980f10c051d
Rachel Keslensky <rach@lid.cc>
2016-08-29 19:40:17 -0700

    Feat: new CSS colors

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
